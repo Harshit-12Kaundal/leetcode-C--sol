8ec9e11f5852319f82d0cc1f9ce211905d0933c1
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2023-02-12 22:58:53 +0530

    Create 2477. Minimum Fuel Cost to Report to the Capital.cpp

---
+++ 2477. Minimum Fuel Cost to Report to the Capital.cpp
@@ -0,0 +1,24 @@
+int f(int ind,int s,int prev,vector<vector<int>>&adj){
+long long int ans=0;
+    int cnt=1;
+    for(auto it: adj[ind]){
+        if(it!=prev){
+            cnt+= f(it,s,ind,adj);
+        }
+    }
+    if(ind!=0){
+        ans+=((cnt+s-1)/s);
+    }
+    return cnt;
+} 
+long long minimumFuelCost(vector<vector<int>>& roads, int seats){
+    int n=roads.size();
+    vector<vector<int>>adj(n+1);
+    for(auto it:roads){
+        adj[it[0]].push_back(it[1]);
+        adj[it[1]].push_back(it[0]);
+    }
+
+    f(0,seats,-1,adj);
+    return ans;
+}
